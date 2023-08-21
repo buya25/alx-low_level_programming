ddb8763f3de5abc158713a0f5b5c710e26afaa17
Yabs Mullo <yabsmullo0@gmail.com>
2023-08-21 01:18:47 -0700

    0x18. C - Dynamic libraries

--- 0x18-dynamic_libraries/101-make_me_win.sh
+++ 0x18-dynamic_libraries/101-make_me_win.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-wget -O /tmp/libdynamic.so https://github.com/buya25/alx-low_level_programming/tree/master/0x18-dynamic_libraries/libdynamic.so
+wget -O /tmp/libdynamic.so https://github.com/buya25/alx-low_level_programming/raw/master/0x18-dynamic_libraries/libdynamic.so
 export LD_PRELOAD=/tmp/libdynamic.so
